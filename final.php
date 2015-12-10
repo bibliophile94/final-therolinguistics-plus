b6c146be986743920dc07ae656c9ddc134ec3be5
Katrina <kstolpe@mail.bradley.edu>
2015-12-10 08:46:02 -0600

    offsetHeight vs client

--- final.php
+++ final.php
@@ -37,8 +37,8 @@ echo "Carve Your Message";
 var initialX = Math.floor(Math.random()*screen.width);
 var initialY = Math.floor(Math.random()*screen.height);
 var texts = document.getElementById("Wrapping");
-var height = (texts.clientHeight)+1;
-var width = (texts.clientWidth)+1;
+var height = (texts.offsetHeight)+1;
+var width = (texts.offsetWidth)+1;
 document.getElementById("Wrapping").style.visibility = "visible";
 
 document.getElementById("Wrapping").style.color = "red";
