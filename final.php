9cd8407ff44948c2c614c2ef244bdb3f6dacd852
Katrina <kstolpe@mail.bradley.edu>
2015-12-10 05:54:22 -0600

    modified proj 3

---
+++ final.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	<link rel="stylesheet" type="text/css" href="final.css">
+        <meta charset="utf-8">
+        <title>The Mossier Tree</title>
+    </head>
+    <body>
+	<?php
+
+if (file_exists('final.txt')) {
+    $content = file_get_contents('final.txt');
+} else {
+    $content = '(no content)';
+}
+if (isset($_GET['content'])) {
+    $content = $_GET['content'];
+    file_put_contents('final.txt', $content);
+}
+$safe_content = htmlentities($content);
+?>
+<div id="content">
+    <?php echo $safe_content; ?>
+</div>
+<form action="final.php">
+    <textarea name="content" rows="1" cols="20"><?php
+
+echo $safe_content;
+
+?></textarea>
+    <input type="submit" value="Save">
+</form>
+<script>
+$('#content').click(function() {
+    $('form').removeClass('hidden');
+    $('#content').addClass('hidden');
+});
+</script>
+</body>
+</html>
