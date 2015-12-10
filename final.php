08ff73f4228b8518bc41b40439c2d62e8b7b2605
Katrina <kstolpe@mail.bradley.edu>
2015-12-10 08:23:48 -0600

    oops, wrong variable name

--- final.php
+++ final.php
@@ -40,8 +40,6 @@ var texts = document.getElementById("Wrapping");
 var height = (texts.clientHeight);
 var width = (texts.clientWidth);
 document.getElementById("Wrapping").style.color = "red";
-
-document.getElementById("body").style.color = "blue";
 console.log(height);
 console.log(width);
 var leftBound = window.innerWidth/2-(width/2);
