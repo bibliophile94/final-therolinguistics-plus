4299bda22a9db833e6aac8235915e85bf99ce141
Katrina <kstolpe@mail.bradley.edu>
2015-12-10 22:03:44 -0600

    changes

--- final.js
+++ final.js
@@ -12,7 +12,7 @@ var leftBound = window.innerWidth/2-(width/2);
 var rightBound = window.innerWidth/2+(width/2);
 var upperBound = window.innerHeight/2+(height/2);
 var lowerBound = window.innerHeight/2-(height/2);
-texts.style.top = lowerBound + "px";
+texts.style.top = upperBound-200 + "px";
 texts.style.left = leftBound + "px"
 console.log(leftBound + " " + rightBound);
 console.log(upperBound + " " + lowerBound);
